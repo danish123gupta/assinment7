bbf9366f3306c074803d991b2f451cd4233c8991
danish123gupta <40190986+danish123gupta@users.noreply.github.com>
2018-10-11 23:03:25 +0530

    Add files via upload

---
+++ jiny.py
@@ -0,0 +1,45 @@
+#q.no.1
+a=3
+if a<4:
+    try:
+        a=a/(a-3)
+        print(a)
+    except Exception as a:
+        print('Zero Divison Error')
+        print(a)
+
+#q.no.2
+list=[1,2,3]
+try:
+    print(list[3])
+except Exception as b:
+    print("index error")
+    print(b)
+
+#q.no.3
+print("Name error: hi there")
+
+#q.no.4
+print("-5.0\n a/b result in 0")
+
+#q.no.5
+#import error
+try:
+    import aba
+except Exception as i:
+    print(i)
+#value error
+try:
+    a=1
+    print(k)
+except Exception as v:
+    print(v)
+
+#index error
+    list = [1, 2, 3]
+    try:
+        print(list[3])
+    except Exception as b:
+        print("index error")
+        print(b)
+
